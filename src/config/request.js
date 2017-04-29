983cd4c0ab48aa80dc06d263e076c19bd39a878e
sky-ADMIN <546485299@qq.com>
2017-04-29 21:17:45 +0800

    补全各种地址

--- src/config/request.js
+++ src/config/request.js
@@ -2,8 +2,14 @@
  * Created by lhy95 on 2017/4/28.
  */
 
-// const baseUrl = 'http://localhost:8086';
-const baseUrl = 'http://easy-mock.com/mock/590080f6875d7232a38b1e80/evaluate/';
+// 全栈工程师本地调试用
+const baseUrl = 'http://localhost:8086';
+
+// 前端工程师测试用
+// const baseUrl = 'http://easy-mock.com/mock/590080f6875d7232a38b1e80/evaluate/';
+
+// 线上地址
+// const baseUrl = 'http://yb.upc.edu.cn:8087';
 
 const API = {
   login: baseUrl + '/instructor/login',
