39554777b67be70600c2b8734306d6c4242c7d75
echaoo <zmk181@gmail.com>
2017-04-29 10:30:00 +0800

    pc端登录

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '../components/Index.vue'
 import Evaluate from '../components/Evaluate.vue'
+import LoginPc from '../components/LoginPc.vue'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
     {
       path: '/evaluate',
       component: Evaluate
+    },
+    {
+      path: '/loginPc',
+      component: LoginPc
     }
   ]
 })
